0200d24b94bbb8981d68738172ec82b3d25d4749
FarahKamleh <fkamleh8800@gmail.com>
2023-11-13 21:05:49 -0600

    Left Aligned
    The details, add, and remove buttons are now aligned to the left. The images for each dish will need to be added to the left.

--- components/DishCard.js
+++ components/DishCard.js
@@ -11,31 +11,33 @@ function DishCard({title, image, description, content, navigation, addCalback = 
     <View style={{backgroundColor: '#fafafa', borderRadius: 8, paddingVertical: 10, paddingHorizontal: 10, width: 400, marginBottom: 10}}>
         <Text style={{color: '#74c476', fontWeight: 'bold', fontSize: 18}}>{title}</Text>
         <Text style= {{color: '#74c476', fontSize: 15}}>{description}</Text>
-        <Button
-            title="Details"
+        <View style={{alignItems: 'left'}}>
+          <Button
+              title="Details"
+              color="#74c476"
+              onPress={() => {
+                navigation.navigate("Dish Details", {
+                    title: title,
+                    description: description,
+                    content: content
+                })
+              }}
+          />
+          <Button 
+            title="Add to Plan"
             color="#74c476"
             onPress={() => {
-              navigation.navigate("Dish Details", {
-                  title: title,
-                  description: description,
-                  content: content
-              })
+              addCalback(title)
             }}
-        />
-        <Button 
-          title="Add to Plan"
-          color="#74c476"
-          onPress={() => {
-            addCalback(title)
-          }}
-        />
-        <Button 
-          title="Remove from Plan"
-          color="#74c476"
-          onPress={() => {
-            removeCallback(title)
-          }}
-        />
+          />
+          <Button 
+            title="Remove from Plan"
+            color="#74c476"
+            onPress={() => {
+              removeCallback(title)
+            }}
+          />
+        </View>
     </View>
   );
 }
